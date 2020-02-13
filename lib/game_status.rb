877c631e796fd0bac5be42e8035f65134415147c
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-13 03:18:15 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,8 @@ WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5
 
 def won?(board)
 
-      if board[0] == "X" && board[1] == "X" && board[2] == "X"     || board[0] == "O" && board[1] == "O" && board[2] == "O"
-           WIN_COMBINATIONS[0]
+      if board[0] == "X" && board[1] == "X" && board[2] == "X" ||   board[0] == "O" && board[1] == "O" && board[2] == "O"
+             WIN_COMBINATIONS[0]
        elsif board[3] == "X" && board[4] == "X" && board[5] == "X" || board[3] == "O" && board[4] == "O" && board[5] == "O"
              WIN_COMBINATIONS[1]
        elsif board[6] == "X" && board[7] == "X" && board[8] == "X" || board[6] == "O" && board[7] == "O" && board[8] == "O"
